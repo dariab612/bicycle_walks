aebf67caecaf89b064cac5178d0d3cdc832dc882
Costa <ancst@yandex.ru>
2021-12-08 16:52:30 +0300

    registration server

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const regRouter = require('./router/registration.router');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(3000);
+app.use(morgan('dev'));
+app.set('view engine', 'hbs');
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/registration', regRouter);
+
+app.listen(PORT, () => {
+  console.log('Server has been shurshed epte', PORT);
+});
